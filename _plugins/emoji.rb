9bb8f82352fedc8c37b609ad76db0377c6c62718
Tammo Ippen <tammo.ippen@posteo.de>
2018-03-26 11:30:44 +0200

    Update emoji stuff

--- _plugins/emoji.rb
+++ _plugins/emoji.rb
@@ -12,8 +12,8 @@
 #   - Copy this file to your `_plugins` directory
 #   - Add a line like `emoji_dir: images/emoji` to your `_config.yml`
 #   - If you want to use external source for emoji, set `emoji_dir: http://...` to your `_config.yml`.
-# 
-# Usage: 
+#
+# Usage:
 #   - Apply the filter wherever needed e.g. {{ content | emojify }}
 #   - Add some emoji to your article! e.g. "Hello :wink:"
 
@@ -33,7 +33,7 @@ module Jekyll
 
       content.to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
         if Emoji.find_by_alias($1) and emoji_dir
-          '<img alt="' + $1 + '" src="/' + emoji_dir + "/#{$1}.png" + '" class="emoji" />'
+          '<img alt="' + $1 + '" src="' + emoji_dir + "/#{$1}.png" + '" class="emoji" />'
         else
           match
         end
@@ -50,7 +50,7 @@ module Jekyll
       emoji_dir = File.join(config['source'], config['emoji_dir'])
       return false if File.exist?(File.join(emoji_dir, 'smiley.png'))
 
-      puts "           Copying: Emoji from Gemoji to " + emoji_dir + " " + config['emoji_dir']
+      puts "           Copying: Emoji from Gemoji to " + config['emoji_dir']
 
       # Make Emoji directory
       FileUtils.mkdir_p(emoji_dir)
